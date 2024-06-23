fc6135c1c64836fcd32281849a9cfd59989416e6
mcrisan5537 <crisanmihai5537@gmail.com>
2024-06-23 22:53:54 +0300

    feat: striptags, jsonld, multiple images, favicons

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import fetch from 'cross-fetch';
 import { parseHTML } from 'linkedom';
+import striptags from 'striptags';
 
 export const parsePreviewDataFromUrl = async (
   url: string,
@@ -16,7 +17,9 @@ export const parsePreviewDataFromUrl = async (
 > => {
   const response = await fetch(url, {
     method: 'GET',
-    headers: options?.headers,
+    headers: {
+      ...options?.headers,
+    },
   });
   const responseHtml = await response.text();
 
@@ -31,10 +34,6 @@ export const parsePreviewDataFromUrl = async (
     ...parsedData,
   };
 };
-
-parsePreviewDataFromUrl(
-  'https://www.westelm.com/products/mp-classic-rattan-wood-buffet-60-h11181/?sku=2321348'
-).then(console.log);
 
 export const parsePreviewDataFromHtml = (
   html: string
@@ -54,7 +53,26 @@ class StructuredDataHtmlParser {
     this.url = url;
   }
 
-  private parseName(): string {
+  private getJsonLd({ type }: { type: string }): Record<string, any> | undefined {
+    return Array.from(
+      this.parseResult.document.querySelectorAll('script[type="application/ld+json"]')
+    )
+      .map((scriptElement) => {
+        if (!scriptElement.textContent) {
+          return;
+        }
+
+        try {
+          return JSON.parse(scriptElement.textContent);
+        } catch (e) {}
+      })
+      .flat(Infinity)
+      .filter((parsedJsonLd) => {
+        return parsedJsonLd['@type']?.toLowerCase() === type.toLowerCase();
+      })?.[0];
+  }
+
+  private parseName(): string | undefined {
     const name =
       this.parseResult.document
         .querySelector<HTMLMetaElement>('meta[property="og:title"]')
@@ -65,13 +83,23 @@ class StructuredDataHtmlParser {
       this.parseResult.document
         .querySelector<HTMLMetaElement>('meta[property="twitter:title"]')
         ?.getAttribute('content') ??
+      this.getJsonLd({
+        type: 'Product',
+      })?.name ??
       this.parseResult.document.title;
 
-    return name;
+    if (name) {
+      return striptags(name).trim();
+    }
   }
 
   private parseImages(): string[] {
+    /**
+     * Handle amazon product pages in a special manner since they're popular
+     * but don't follow the usual meta tags for images
+     */
     try {
+      // TODO: include other amazon regional domains
       if (this.url && ['www.amazon.com'].includes(new URL(this.url).hostname)) {
         const scriptElement = this.parseResult.document.querySelector<HTMLScriptElement>(
           'script[data-a-state*="desktop-landing-image-data"]'
@@ -85,44 +113,50 @@ class StructuredDataHtmlParser {
       }
     } catch (e) {}
 
-    const image =
-      this.parseResult.document
-        .querySelector<HTMLMetaElement>('meta[property="og:image:secure_url"]')
-        ?.getAttribute('content') ??
-      this.parseResult.document
-        .querySelector<HTMLMetaElement>('meta[property="og:image:url"]')
-        ?.getAttribute('content') ??
-      this.parseResult.document
-        .querySelector<HTMLMetaElement>('meta[property="og:image"]')
-        ?.getAttribute('content') ??
-      this.parseResult.document
-        .querySelector<HTMLMetaElement>('meta[name="twitter:image:src"]')
-        ?.getAttribute('content') ??
-      this.parseResult.document
-        .querySelector<HTMLMetaElement>('meta[property="twitter:image:src"]')
-        ?.getAttribute('content') ??
-      this.parseResult.document
-        .querySelector<HTMLMetaElement>('meta[name="twitter:image"]')
-        ?.getAttribute('content') ??
-      this.parseResult.document
-        .querySelector<HTMLMetaElement>('meta[property="twitter:image"]')
-        ?.getAttribute('content') ??
-      this.parseResult.document
-        .querySelector<HTMLMetaElement>('meta[itemprop="image"]')
-        ?.getAttribute('content') ??
-      this.parseResult.document.querySelector<HTMLImageElement>('article img[src]')?.src ??
-      this.parseResult.document.querySelector<HTMLImageElement>('#content img[src]')?.src ??
-      this.parseResult.document.querySelector<HTMLImageElement>('img[alt*="author" i]')?.src ??
-      this.parseResult.document.querySelector<HTMLImageElement>(
-        'img[src]:not([aria-hidden="true"])'
-      )?.src;
+    const imageMetaTags = [
+      this.parseResult.document.querySelectorAll<HTMLMetaElement>(
+        'meta[property="og:image:secure_url"]'
+      ),
+      this.parseResult.document.querySelectorAll<HTMLMetaElement>(
+        'meta[property="og:image:secure_url"]'
+      ),
+      this.parseResult.document.querySelectorAll<HTMLMetaElement>('meta[property="og:image:url"]'),
+      this.parseResult.document.querySelectorAll<HTMLMetaElement>('meta[property="og:image"]'),
+      this.parseResult.document.querySelectorAll<HTMLMetaElement>('meta[name="twitter:image:src"]'),
+      this.parseResult.document.querySelectorAll<HTMLMetaElement>(
+        'meta[property="twitter:image:src"]'
+      ),
+      this.parseResult.document.querySelectorAll<HTMLMetaElement>('meta[name="twitter:image"]'),
+      this.parseResult.document.querySelectorAll<HTMLMetaElement>('meta[property="twitter:image"]'),
+      this.parseResult.document.querySelectorAll<HTMLMetaElement>('meta[itemprop="image"]'),
+    ]
+      .map((nodeList) => Array.from(nodeList))
+      .flat(Infinity)
+      .map((metatag) => (metatag as any)?.getAttribute?.('content') as string | undefined);
 
-    // TODO: use `querySelectorAll()` to get all images
-    if (image) {
-      return [image];
-    }
+    const jsonLdProductImages = this.getJsonLd({
+      type: 'Product',
+    })?.image;
 
-    return [];
+    const images = [
+      ...imageMetaTags,
+      ...(Array.isArray(jsonLdProductImages) ? jsonLdProductImages : []),
+    ].filter((content): content is string => typeof content === 'string');
+
+    return [...new Set(images)];
+  }
+
+  private parseFavicons(): string[] {
+    const faviconLinkTags = [
+      this.parseResult.document.querySelectorAll<HTMLMetaElement>('link[rel="icon"]'),
+      this.parseResult.document.querySelectorAll<HTMLMetaElement>('link[rel="shortcut icon"]'),
+      this.parseResult.document.querySelectorAll<HTMLMetaElement>('link[rel="apple-touch-icon"]'),
+    ]
+      .map((nodeList) => Array.from(nodeList))
+      .flat(Infinity)
+      .map((metatag) => (metatag as any)?.getAttribute?.('href') as string | undefined);
+
+    return faviconLinkTags.filter((content): content is string => typeof content === 'string');
   }
 
   private parseDescription(): string | undefined {
@@ -144,62 +178,59 @@ class StructuredDataHtmlParser {
         ?.getAttribute('content') ??
       this.parseResult.document
         .querySelector<HTMLMetaElement>('meta[itemprop="description"]')
-        ?.getAttribute('content');
+        ?.getAttribute('content') ??
+      this.getJsonLd({ type: 'Product' })?.description;
 
     if (description) {
-      return description;
+      return striptags(description).trim();
     }
   }
 
-  private parsePrice(): number | undefined {
-    const price =
+  private parsePriceAmount(): number | undefined {
+    let priceAmount: string =
       this.parseResult.document
         .querySelector<HTMLMetaElement>('meta[property="og:price:amount"]')
         ?.getAttribute('content') ??
       this.parseResult.document
         .querySelector<HTMLImageElement>('meta[property="product:price:amount"]')
         ?.getAttribute('content') ??
-      this.parseResult.document
-        .querySelector<HTMLMetaElement>('meta[property="twitter:data1"]')
-        ?.getAttribute('content') ??
-      this.parseResult.document
-        .querySelector<HTMLMetaElement>('meta[property="twitter:label1"]')
-        ?.getAttribute('content') ??
-      this.parseResult.document
-        .querySelector<HTMLMetaElement>('meta[property="twitter:data2"]')
-        ?.getAttribute('content') ??
-      this.parseResult.document
-        .querySelector<HTMLMetaElement>('meta[property="twitter:label2"]')
-        ?.getAttribute('content') ??
-      this.parseResult.document
-        .querySelector<HTMLImageElement>('meta[property="product:price:currency"]')
-        ?.getAttribute('content');
+      this.getJsonLd({ type: 'Product' })?.offers?.price ??
+      this.getJsonLd({ type: 'Product' })?.offers?.lowPrice ??
+      this.getJsonLd({ type: 'Product' })?.offers?.[0]?.price;
 
-    if (price) {
-      return Number.parseFloat(price);
+    if (priceAmount) {
+      if (priceAmount.includes(',') && priceAmount.includes('.')) {
+        priceAmount = priceAmount.replace(',', '');
+        return Number.parseFloat(priceAmount);
+      }
+
+      return Number.parseFloat(priceAmount);
     }
 
     return undefined;
   }
 
-  private parseCurrency(): string | undefined {
-    const currency =
+  private parsePriceCurrency(): string | undefined {
+    const priceCurrency =
+      this.parseResult.document.querySelector<HTMLMetaElement>(
+        'meta[property="og:price:currency"]'
+      )?.['content'] ??
       this.parseResult.document.querySelector<HTMLMetaElement>(
         'meta[property="product:price:currency"]'
       )?.['content'] ??
-      this.parseResult.document.querySelector<HTMLMetaElement>(
-        'meta[property="og:price:currency"]'
-      )?.['content'];
+      this.getJsonLd({ type: 'Product' })?.offers?.priceCurrency ??
+      this.getJsonLd({ type: 'Product' })?.offers?.[0]?.priceCurrency;
 
-    if (currency) {
-      return currency;
+    if (priceCurrency) {
+      return priceCurrency;
     }
   }
 
   private parseSiteName(): string | undefined {
-    const siteName = this.parseResult.document.querySelector<HTMLMetaElement>(
-      'meta[property="og:site_name"]'
-    )?.['content'];
+    const siteName =
+      this.parseResult.document.querySelector<HTMLMetaElement>('meta[property="og:site_name"]')?.[
+        'content'
+      ] ?? this.getJsonLd({ type: 'WebSite' })?.name;
 
     if (siteName) {
       return siteName;
@@ -211,8 +242,9 @@ class StructuredDataHtmlParser {
       name: this.parseName(),
       description: this.parseDescription(),
       images: this.parseImages(),
-      price: this.parsePrice(),
-      currency: this.parseCurrency(),
+      favicons: this.parseFavicons(),
+      priceAmount: this.parsePriceAmount(),
+      priceCurrency: this.parsePriceCurrency(),
       siteName: this.parseSiteName(),
     };
   }
